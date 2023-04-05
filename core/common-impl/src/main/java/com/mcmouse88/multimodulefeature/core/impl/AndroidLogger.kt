7c79ef24c3f828e7aa4c49222afff34416b3381e
razdobudko.s <razdobudko.s@wb.ru>
2023-04-05 23:05:25 +0200

    Fill core common-impl

--- core/common-impl/src/main/java/com/mcmouse88/multimodulefeature/core/impl/AndroidLogger.kt
+++ core/common-impl/src/main/java/com/mcmouse88/multimodulefeature/core/impl/AndroidLogger.kt
@@ -1,4 +1,15 @@
 package com.mcmouse88.multimodulefeature.core.impl
 
-class AndroidLogger {
+import android.util.Log
+import com.mcmouse88.multimodulefeature.core.common.Logger
+
+class AndroidLogger : Logger {
+
+    override fun log(message: String) {
+        Log.d(javaClass.simpleName, message)
+    }
+
+    override fun err(exception: Throwable, message: String?) {
+        Log.e(javaClass.simpleName, message, exception)
+    }
 }
--- core/common-impl/src/main/java/com/mcmouse88/multimodulefeature/core/impl/DefaultGlobalScope.kt
+++ core/common-impl/src/main/java/com/mcmouse88/multimodulefeature/core/impl/DefaultGlobalScope.kt
@@ -1,1 +1,14 @@
 package com.mcmouse88.multimodulefeature.core.impl
+
+import android.util.Log
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+
+fun createDefaultGlobalScope(): CoroutineScope {
+    val errorHandler = CoroutineExceptionHandler { _, exception ->
+        Log.e("DefaultGlobalScope", "Error!!", exception)
+    }
+    return CoroutineScope(SupervisorJob() + Dispatchers.Main + errorHandler)
+}
